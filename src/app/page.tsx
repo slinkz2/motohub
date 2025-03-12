861231e126a99fd9b7ec06a4e2f0de4753e71687
xdjklr <reyeskyle@gmail.com>
2025-03-12 23:51:45 +0800

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://5o49s9j5d8.ufs.sh/f/AYfuaRWx9vQliuFquAamPFXcUezlCH19IOLq87tryNpbJjAo",
+  
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+   id: index + 1,
+   url,
+}))
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      hello
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id}className="w-60">
+              <img src={image.url} alt="" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
