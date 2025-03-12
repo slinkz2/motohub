96cfe198be82d5e4bbf81a664a8e503e1020a91d
xdjklr <reyeskyle@gmail.com>
2025-03-13 02:24:02 +0800

    image database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,9 @@ import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
-const mockUrls = [
-  "https://5o49s9j5d8.ufs.sh/f/AYfuaRWx9vQliuFquAamPFXcUezlCH19IOLq87tryNpbJjAo",
-]
 
-const mockImages = mockUrls.map((url, index) => ({
-   id: index + 1,
-   url,
-}))
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
 
   return (
     <main 
@@ -20,15 +13,14 @@ export default async function HomePage() {
     >
       <div className="flex flex-wrap gap-4 p-8">
         
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div key={image.id} className="w-60">
-            <img src={image.url} alt="" className="w-full h-auto" />
+            <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
 
-      {posts.map((post) =>(
-        <div key={post.id}>{post.name}</div>
-      ))}
+     
       
                 
       </div>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,11 +18,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `motohub_${name}`);
 
-export const posts = createTable(
+export const images = createTable(
   "post",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url",{length:1024}).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
